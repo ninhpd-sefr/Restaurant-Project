d2957283a750d94ebc9e4ccc91c4af2491ae27a9
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2022-10-05 22:52:57 +0700

    awards

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs'
+import { meal } from '../../constants'
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const vidRef = React.useRef();
+  const [playVideo,setPlayVideo] = useState(false)
+
+
+  const handleVideo = () =>{
+    setPlayVideo(!playVideo)
+
+    if(playVideo){
+      vidRef.current.pause();
+    }else{
+      vidRef.current.play();
+    }
+  }
+  return (
+    <div className='app__video'>
+      <video
+        src={meal}
+        ref={vidRef}
+        type='video/mp4'
+        controls={false}
+        Loop
+        muted
+      />
+
+      <div className='app__video-overlay flex__center'>
+        <div
+        onClick={handleVideo}
+         className='app__video-overlay_circle flex__center'>
+          {
+            playVideo? (
+              <BsPauseFill color='#fff' fontSize={30} />
+            ): (
+              <BsFillPlayFill   color='#fff' fontSize={30}/>
+            )
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Intro;
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-
+import { images, data } from '../../constants'
+import { SubHeading } from '../../components';
 import './Laurels.css';
 
+
+
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding' id='awards'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Awards & recognition' />
+      <h1 className='headtext__cormorant'>Our Laurels</h1>
+
+
+      <div className='app__laurels_awards'>
+        {
+          data.awards.map((award, index) => (
+            <div className='app__laurels_awards-card'>
+              <img src={award.imgUrl} alt='award' />
+              <div className='app__laurels_awards-card_content'>
+                <p className='p__cormorant' style={{ color: '#DCCA87' }}>{award.title}</p>
+                <p className='p__cormorant'>{award.subtitle}</p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+
+    <div className='app__wapper_img'>
+      <img src={images.laurels} alt='laurels' />
+    </div>
   </div>
 );
 
